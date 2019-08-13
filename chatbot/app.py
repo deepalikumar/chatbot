fe1e33683963bd56f2fe07a5e9c406e40ef490ae
Deepali Kumar <d@acnt.me>
2019-08-13 20:58:33 +0530

    Fix development server

--- chatbot/app.py
+++ chatbot/app.py
@@ -1,7 +1,6 @@
 """Application definition."""
 from bocadillo import App, discover_providers
 
-
 app = App()
 discover_providers("chatbot.providerconf")
 
@@ -10,3 +9,8 @@ discover_providers("chatbot.providerconf")
 async def converse(ws):
     async for message in ws:
         await ws.send(message)
+
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run("chatbot.asgi:app", host="0.0.0.0", reload=True)
